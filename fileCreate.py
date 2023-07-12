15b35b979ef2e1719952da891685711dbf8230a6
sangh98 <119509793+sangh98@users.noreply.github.com>
2023-07-12 20:58:52 +0900

    Update fileCreate.py

--- fileCreate.py
+++ fileCreate.py
@@ -2,14 +2,14 @@ import os
 
 # 폴더 경로 설정
 folder_path = "txt"
-os.makedirs(folder_path, exist_ok=True)  # 폴더가 없는 경우 생성합니다.
+os.makedirs(folder_path, exist_ok=True)
 
 for i in range(1, 11):
     for j in range(1, 11):
-        number1 = str(i).zfill(2)  # 숫자를 2자리로 만들기 위해 zfill() 함수를 사용합니다.
+        number1 = str(i).zfill(2) 
         number2 = str(j).zfill(2)
         filename = f"test{number1}{number2}.txt"
-        filepath = os.path.join(folder_path, filename)  # 폴더 경로와 파일 이름을 결합합니다.
+        filepath = os.path.join(folder_path, filename)
 
         with open(filepath, "w", encoding=("UTF-8")) as file:
             # 파일 작성 내용
